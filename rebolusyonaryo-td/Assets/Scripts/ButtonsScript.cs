b5c61c541caf5d05ff4a457cf91a23184d95c05d
alvinpanerio <panerio.alvin18@gmail.com>
2023-01-02 23:59:04 +0800

    Adding rounds [NOT OPTIMIZED]

---
+++ rebolusyonaryo-td/Assets/Scripts/ButtonsScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonsScript : MonoBehaviour
+{
+    public SpawnEnemyScipt spawnEnemyScipt;
+
+    void Start() { }
+
+    void Update() { }
+
+    
+}
--- rebolusyonaryo-td/Assets/Scripts/SpawnEnemyScipt.cs
+++ rebolusyonaryo-td/Assets/Scripts/SpawnEnemyScipt.cs
@@ -1,31 +1,92 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpawnEnemyScipt : MonoBehaviour
 {
     public GameObject[] enemySoldiers;
     public GameObject[] instantiatedEnemySoldiers;
     public GameObject spawnLocation;
+    public Button[] defenderButtons;
+    private bool isReadyToPlay = false;
+    private bool isSpawnAvailable = true;
+    private int roundNum = 0;
+
+    private int instantiatedEnemySoldiersCount = 0;
+
+    public GameObject roundText;
 
     void Start() { }
 
     void Update()
     {
-        delaySpawn();
+        changeRoundText();
+        readyToSpawn();
         storeEnemySoldier();
+        ifEnemyZero();
+    }
+
+    public void playButton()
+    {
+        if (!isReadyToPlay)
+        {
+            roundNum++;
+            isReadyToPlay = !isReadyToPlay;
+            foreach (var btn in defenderButtons)
+            {
+                btn.interactable = false;
+            }
+        }
+    }
+
+    void readyToSpawn()
+    {
+        if (isReadyToPlay)
+        {
+            if (isSpawnAvailable)
+            {
+                delaySpawn();
+                storeEnemySoldier();
+            }
+        }
     }
 
     //spawing enemy soldiers
     void spawnEnemySoldiers()
     {
-        var randomNum = Random.Range(0, 7);
-        // var randomNum = 4;
-        Instantiate(
-            enemySoldiers[randomNum],
-            spawnLocation.transform.position,
-            spawnLocation.transform.rotation
-        );
+        if (roundNum == 1)
+        {
+            var randomNum = Random.Range(0, 2);
+            Instantiate(
+                enemySoldiers[randomNum],
+                spawnLocation.transform.position,
+                spawnLocation.transform.rotation
+            );
+
+            instantiatedEnemySoldiersCount++;
+
+            if (instantiatedEnemySoldiersCount == 10)
+            {
+                isSpawnAvailable = !isSpawnAvailable;
+            }
+        }
+        else if (roundNum == 2)
+        {
+            var randomNum = Random.Range(0, 3);
+            Instantiate(
+                enemySoldiers[randomNum],
+                spawnLocation.transform.position,
+                spawnLocation.transform.rotation
+            );
+
+            instantiatedEnemySoldiersCount++;
+
+            if (instantiatedEnemySoldiersCount == 20)
+            {
+                isSpawnAvailable = !isSpawnAvailable;
+            }
+        }
     }
 
     //delay spawn
@@ -42,5 +103,30 @@ public class SpawnEnemyScipt : MonoBehaviour
     void storeEnemySoldier()
     {
         instantiatedEnemySoldiers = GameObject.FindGameObjectsWithTag("EnemySoldiers");
+    }
+
+    void ifEnemyZero()
+    {
+        if (!isSpawnAvailable)
+        {
+            if (isReadyToPlay)
+            {
+                if (instantiatedEnemySoldiers.Length == 0)
+                {
+                    isReadyToPlay = !isReadyToPlay;
+                    foreach (var btn in defenderButtons)
+                    {
+                        btn.interactable = true;
+                    }
+                    isSpawnAvailable = !isSpawnAvailable;
+                    instantiatedEnemySoldiersCount = 0;
+                }
+            }
+        }
+    }
+
+    void changeRoundText()
+    {
+        roundText.gameObject.GetComponent<TMPro.TextMeshProUGUI>().text = roundNum.ToString();
     }
 }
